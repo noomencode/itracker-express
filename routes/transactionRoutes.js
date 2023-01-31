00402119ea60373df922262c0c1dcf8be0052e48
Siim Noomen <siimnoomen@gmail.com@>
2023-01-31 19:49:51 +0200

    added get route

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { addTransaction } from "../controllers/transactionController.js";
+import {
+  addTransaction,
+  getTransactions,
+} from "../controllers/transactionController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").get(protect, getTransactions);
 router.route("/").post(protect, addTransaction);
 
 export default router;
