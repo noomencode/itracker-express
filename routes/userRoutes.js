fd979b4788e7beb79e5ebd8e32398557afba45c4
sinoom <sinoom@ttu.ee>
2023-04-12 19:46:37 +0300

    cwp routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,13 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  changePassword,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.route("/").post(registerUser);
-router.post("/login", authUser);
+router.route("/login").post(authUser);
+router.route("/changePassword").post(protect, changePassword);
 router.route("/profile").get(protect, getUserProfile);
 export default router;
